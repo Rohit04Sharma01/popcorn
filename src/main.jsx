450cd9d561be3aa82700c00abace3716adc6741e
Rohit Sharma <rohit04sharm10@gmail.com>
2023-12-22 16:02:41 +0530

    star component done

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+// import "./index.css";
+import Star from "./components/Star.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    {/* <App /> */}
+    <Star height={150} width={150} color="red" star={10} />
+  </React.StrictMode>
+);
